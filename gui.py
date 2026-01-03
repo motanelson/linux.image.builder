0a8d0e058a2a0503fd7f31ee569809bce19b331d
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-01-03 10:31:32 +0000

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,119 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from tkinter.scrolledtext import ScrolledText
+import os
+
+class RamDiskBuilderGUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("linux Builder...")
+        self.root.geometry("800x600")
+        self.root.configure(bg="black")
+
+        # Memória onde os ficheiros são ligados
+        self.memory_buffer = b""
+        self.loaded_files = []
+
+        # Barra superior com botões
+        top = tk.Frame(root, bg="black")
+        top.pack(fill="x", pady=5)
+
+        btn_style = {
+            "bg": "white",
+            "fg": "black",
+            "activebackground": "#dddddd",
+            "activeforeground": "black",
+            "relief": "flat",
+            "width": 10
+        }
+
+        tk.Button(top, text="OPEN", command=self.open_files, **btn_style).pack(side="left", padx=5)
+        tk.Button(top, text="SAVE", command=self.save_file, **btn_style).pack(side="left", padx=5)
+        tk.Button(top, text="CLEAR", command=self.clear_memory, **btn_style).pack(side="left", padx=5)
+
+        # Área de texto (preview/debug)
+        self.text = ScrolledText(
+            root,
+            bg="black",
+            fg="white",
+            insertbackground="white",
+            font=("Consolas", 10)
+        )
+        self.text.pack(fill="both", expand=True, padx=5, pady=5)
+
+        self.log("linux Builder startup.\n")
+
+    def log(self, msg):
+        self.text.insert("end", msg)
+        self.text.see("end")
+
+    def open_files(self):
+        paths = filedialog.askopenfilenames(
+            title="open files ",
+            filetypes=[("elf files", "*"), ("All files", "*")]
+        )
+        if 0==0:
+                os.system("mkdir /mnt/rams > /dev/null")
+                os.system("rm initrd > /dev/null")
+                os.system("cp init initrd > /dev/null")
+                os.system("umount /mnt/rams > /dev/null")
+                os.system("mount -o loop initrd /mnt/rams > /dev/null")
+                
+        if not paths:
+            return
+
+        for path in paths:
+            #try:
+            if 0==0:
+                os.system("cp $1 /mnt/rams ".replace("$1",path))
+                
+                self.log(f"[OK] Carregado: {path} \n")
+            #except Exception as e:
+            #    self.log(f"[ERRO] {path}: {e}\n")
+        os.system("chmod 777 /mnt/rams/* ")
+        os.system("umount /mnt/rams > /dev/null")
+        
+
+    def save_file(self):
+        
+
+        path = filedialog.asksaveasfilename(
+            title="save disk",
+            defaultextension=".iso",
+            filetypes=[("iso", "*.iso"), ("All files", "*")]
+        )
+
+        if not path:
+            return
+
+        try:
+            n=path
+            os.system("umount /mnt/rams > /dev/null")
+            os.system('dd if=/dev/zero of="$1" bs=1M count=15 status=progress'.replace("$1",n))
+            os.system("mkfs.fat -F 12 $1".replace("$1",n))
+            os.system('syslinux "$1"'.replace("$1",n))
+            os.system("rm initrd.gz > /dev/null")
+            os.system("gzip initrd > /dev/null")
+            os.system('mcopy -i "$1" syslinux.cfg ::/syslinux.cfg'.replace("$1",n))
+            os.system('mcopy -i "$1" initrd.gz ::/initrd.gz'.replace("$1",n))
+            os.system('mcopy -i "$1" vmlinuz ::/vmlinuz'.replace("$1",n))
+
+        except Exception as e:
+            messagebox.showerror("Erro", str(e))
+
+    def clear_memory(self):
+        self.memory_buffer=""
+        self.loaded_files.clear()
+        self.text.delete("1.0", "end")
+        self.log("memory clear.\n")
+        if 0==0:
+                os.system("mkdir /mnt/rams > /dev/null")
+                os.system("cp init initrd > /dev/null")
+                os.system("umount /mnt/rams > /dev/null")
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = RamDiskBuilderGUI(root)
+    root.mainloop()
